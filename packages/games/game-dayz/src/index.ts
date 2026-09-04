77efc485b194a60bbdabcc73ab4b2f8b43de53c3
Carlos Menezes <carlos.dm.menezes@gmail.com>
2026-09-04 12:50:32 +0100

    fix: build

--- packages/games/game-dayz/src/index.ts
+++ packages/games/game-dayz/src/index.ts
@@ -1,6 +1,3 @@
-import { createValveProtocol } from "@srvquery/protocol-valve";
-import { dayzRuleParser } from "./rules";
-
 export { dayzRuleParser } from "./rules";
 export {
   DayZDlcSchema,
@@ -10,16 +7,3 @@ export {
   type DayZMod,
   type DayZRules,
 } from "./schema";
-
-const proto = createValveProtocol({
-  host: "168.100.162.38",
-  port: 27016,
-});
-{
-  const info = await proto.query("INFO");
-  console.log(info);
-  const rules = await proto.query("RULES", { parser: dayzRuleParser });
-  console.log(rules);
-  const players = await proto.query("PLAYERS");
-  console.log(players);
-}
